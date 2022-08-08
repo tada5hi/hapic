89a12cca3aefa8ceceae3a269ac1382314d88bc1
tada5hi <peter.placzek1996@gmail.com>
2022-08-08 13:17:56 +0200

    fixed version range for hapic

--- packages/client/src/utils/has-own-property.ts
+++ packages/client/src/utils/has-own-property.ts
@@ -6,6 +6,9 @@
  */
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function hasOwnProperty<X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> {
+export function hasOwnProperty<X extends {}, Y extends PropertyKey>(
+    obj: X,
+    prop: Y,
+): obj is X & Record<Y, unknown> {
     return Object.prototype.hasOwnProperty.call(obj, prop);
 }
