bb68cba110b48693d9dd99c8178afaa33a285623
tada5hi <peter.placzek1996@gmail.com>
2023-03-17 10:00:09 +0100

    feat(vault): expose hapic api + custom create-,set,use-client utilities

---
+++ packages/vault/src/client.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2023.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import type {
+    Config,
+} from 'hapic';
+import {
+    setClient as _setClient,
+    useClient as _useClient,
+    buildConfig,
+} from 'hapic';
+import { Client } from './module';
+
+export type {
+    Config,
+
+    ClientRequestConfig,
+    ClientResponse,
+    ClientInterface,
+    ClientDriverInstance,
+    ClientError,
+    ClientRetryConfig,
+
+    ErrorCode,
+
+} from 'hapic';
+export {
+    buildConfig,
+    setConfig,
+    useConfig,
+} from 'hapic';
+
+export function createClient(input?: Omit<Config, 'clazz'>) : Client {
+    const config = buildConfig(input);
+
+    return new Client(config);
+}
+
+export function setClient<T extends Client = Client>(client: T, key?: string) : T {
+    _setClient(client, key);
+
+    return client;
+}
+
+export function useClient(key?: string) : Client {
+    key = key || 'vault';
+
+    let client = _useClient(key);
+    if (client instanceof Client) {
+        return client;
+    }
+
+    client = createClient();
+    setClient(client as Client, key);
+
+    return client as Client;
+}
--- packages/vault/src/index.ts
+++ packages/vault/src/index.ts
@@ -5,6 +5,7 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
+export * from './client';
 export * from './error';
 export * from './module';
 export * from './mount';
